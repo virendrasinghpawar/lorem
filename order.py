80f98934ff75ef1a96462e228a81c45231778007
Virendra Singh Pawar <virendrasinghpawar@gmail.com>
2018-04-11 22:54:41 +0530

    first commit

---
+++ order.py
@@ -0,0 +1,90 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import xlsxwriter
+from dateutil import parser
+url = "https://www.homeshop18.com/account/orderDetails"
+from variable import *
+workbook = xlsxwriter.Workbook("HS18.xlsx")
+workbook.formats[0].set_font_size(8)
+
+worksheet = workbook.add_worksheet()
+payload = ""
+formate=workbook.add_format()
+formate.set_bold()
+formate.set_font_color('red')
+
+worksheet.set_column(0, 0, 10)
+worksheet.set_column(1,1,16)
+worksheet.set_column(2,2,18)
+worksheet.set_column(3,3,10)
+worksheet.set_column(4,4,40)
+worksheet.set_column(5,5,9)
+worksheet.set_column(6,6,32)
+worksheet.set_column(7,7,16)
+
+worksheet.write_string  (0, 0,     "pid",formate)
+worksheet.write_string  (0, 1,     "Date" ,formate)
+worksheet.write_string  (0, 2,     "Name",formate)
+worksheet.write_string  (0, 3,     "Contact",formate)
+worksheet.write_string  (0, 4,     "Ship Detail",formate)
+worksheet.write_string  (0, 5,     "Price",formate)
+worksheet.write_string  (0, 6,     "Product Summary",formate)
+worksheet.write_string  (0, 7,     "Company",formate)
+
+
+row=1
+for i in range(0,5):
+    orderId=orderId+2
+    querystring = {"orderId":orderId}
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    soup = BeautifulSoup(response.content)
+    notFound=soup.find_all('div',class_="sorry-content")
+    if notFound:
+        print(i)
+    else:
+        print(i)
+        orderDetails = soup.find_all('div',class_="order-section-row1 clearfix")
+        pid=orderDetails[0].find_all('div')[0].find('p').text
+        pdate=orderDetails[0].find_all('div')[1].find('p').text
+        dt = parser.parse(pdate)
+        dt=(str(dt.year)+"-"+str(dt.month)+"-"+str(dt.day) )
+        # print(dt)
+        # time.sleep(10)
+        shippingDetails= soup.find_all('div',class_="col col-shiping")
+        # print(shippingDetails)
+        shipdetail=' '.join( shippingDetails[0].find('p').text.split())
+        shipdetail=shipdetail.split("Mobile")[0].title()
+        # print(shipdetail)
+
+
+        Mobile=''.join( shippingDetails[0].find('p').text.split()).split(':')[-1]
+
+        name=shippingDetails[0].find('p').find('strong').text
+        # print(name)
+
+        paymentDetails=soup.find_all('div',class_="col col-payment last")
+        paymentDetail=''.join( paymentDetails[0].find('p').text.split()).split(':')[-1]
+        paymentDetail="Rs."+paymentDetail.split('.')[0]
+        # paymentDetail="Rs."+str(paymentDetail)
+        print(paymentDetail)
+        productSummary=soup.find_all('td',valign="middle")
+        productsumm=''
+        for product in productSummary:
+            if product.find('a'):
+                productsumm=productsumm+product.find('a').text
+        # print(pid)
+        # print(productsumm)
+        worksheet.write_string  (row, 0,     pid)
+        worksheet.write_string  (row, 1,     pdate )
+        worksheet.write_string  (row, 2,     name)
+        worksheet.write_string  (row, 3,     Mobile)
+        worksheet.write_string  (row, 4,     shipdetail)
+        worksheet.write_string  (row, 5,     paymentDetail)
+        worksheet.write_string  (row, 6,     productsumm)
+        worksheet.write_string  (row, 7,     "HOMESHOP")
+        
+        
+        row=row+1
+workbook.close()     
---
+++ variable.py
@@ -0,0 +1,19 @@
+headers = {
+    'origin': "https://www.homeshop18.com",
+    'upgrade-insecure-requests': "1",
+    'content-type': "application/x-www-form-urlencoded",
+    'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/63.0.3239.84 Chrome/63.0.3239.84 Safari/537.36",
+    'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    'referer': "https://www.homeshop18.com/login.html",
+    'accept-encoding': "gzip, deflate, br",
+    'accept-language': "en-GB,en-US;q=0.9,en;q=0.8",
+    'cookie': "_ga=GA1.2.200097211.1516739790; __auc=76ed7ade16124bc83a308fe73e1; __utmz=29599654.1516739790.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _vz=viz_5a679cd279649; JSESSIONID=0416A89D26E49E0E7AFB67B23BD4E753-n2.jvmnode7; bannerClosed=true; _gid=GA1.2.1302961489.1522950017; __utmc=29599654; HS_ID=52704e28a0c345d6777b1ac25b4bd4fbdf7f2dca5b4abee06a488d474edf257f; rvp=33018081; __asc=74add74d1629871730217dbc90f; _gat_UA-2260539-1=1; __utma=29599654.200097211.1516739790.1522950018.1522975996.36; __utmb=29599654.0.10.1522975996; ssl_ID=6e6cc715-680e-4eee-8263-c5c69b154d13; izpt=1; __ar_v4=BWGGVHU26BD3TNCKVLZLJR%3A20180406%3A2%7C46SY4BMC4RBVZJZURTM4R2%3A20180406%3A2%7CZTA2QFNS6ZFANOB6FX4YHX%3A20180406%3A2",
+    'cache-control': "no-cache",
+    'hacscript-token': "2460041e-9f48-a52e-0fce-2e67d4b989e1",
+    'main_token':'4082400-GHJLJGLS-14197481-QOWUOEU'
+    }
+
+
+orderId=1021894693
+
+filename='virendra123AS.xlsx';
